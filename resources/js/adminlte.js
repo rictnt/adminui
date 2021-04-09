24d2fe1f4a1b0598eec0a8e5f4f959e99dc2aea1
rich tonato <rictnt@gmail.com>
2021-04-08 21:39:49 -0500

    first commit

---
+++ resources/js/adminlte.js
@@ -0,0 +1,12 @@
+require ('admin-lte/plugins/overlayScrollbars/js/OverlayScrollbars.min')
+require ('admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min')
+// require('overlayscrollbars/js/jquery.overlayScrollbars.min')
+require('admin-lte')
+require('admin-lte/plugins/jquery/jquery.min')
+require('admin-lte/plugins/datatables/jquery.dataTables.min')
+require('admin-lte/plugins/datatables-bs4/js/dataTables.bootstrap4.min')
+require('admin-lte/plugins/datatables-responsive/js/dataTables.responsive.min')
+require('admin-lte/plugins/datatables-responsive/js/responsive.bootstrap4.min')
+require('admin-lte/plugins/select2/js/select2.full.min')
+
+$.widget.bridge('uibutton', $.ui.button);
